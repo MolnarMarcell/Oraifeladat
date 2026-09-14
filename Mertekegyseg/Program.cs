a945b2dbd06da726f9e281616dea1dadc33e0a75
MolnarMarcell <marci20070208@gmail.com>
2026-09-14 10:34:26 +0200

    2. feladat: Mértékegység Átváltó

---
+++ Mertekegyseg/Program.cs
@@ -0,0 +1,11 @@
+﻿using Mertekegyseg;
+
+double meter = Atvalto.KmBolMeter(5);
+Console.WriteLine(meter);
+
+Atvalto Atvaltas = new Atvalto();
+
+Atvaltas.Szorzo = 400;
+
+double eredmeny = Atvaltas.EgyediAtvaltas(5);
+Console.WriteLine(eredmeny);
