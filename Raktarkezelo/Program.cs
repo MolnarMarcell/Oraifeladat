23fab1fe36818511d6350b035814c364a8ec3ee7
MolnarMarcell <marci20070208@gmail.com>
2026-09-14 11:08:52 +0200

    4. feladat

---
+++ Raktarkezelo/Program.cs
@@ -0,0 +1,11 @@
+﻿using Raktarkezelo;
+
+RaktarElem elem = new RaktarElem("Cikk1", 10);
+
+elem.Bevetelezes(5);
+
+elem.Kiadas(3);
+
+double bruttoAr = RaktarElem.BruttoArSzamitas(100);
+
+Console.WriteLine(bruttoAr);
---
+++ Raktarkezelo/RaktarElem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raktarkezelo
+{
+    internal class RaktarElem
+    {
+        private int dbSzam = 0;
+        public string CikkNev { get; set; }
+
+        public static double AfaKulcs = 0.27;
+
+        public void Bevetelezes(int db)
+        {
+            if (db > 0) {
+                dbSzam += db;
+                Console.WriteLine("Bevételezés rögzítve.");
+            }
+            else
+            {
+                Console.WriteLine("Hibás bevételezés!");
+            }
+        }
+
+        public void Kiadas(int db)
+        {
+            if (db > 0 && db <= dbSzam) {
+                dbSzam -= db;
+                Console.WriteLine("Kiadás rögzítve.");
+            }
+            else
+            {
+                Console.WriteLine("Hibás kiadás!");
+            }
+        }
+
+        public static double BruttoArSzamitas(double nettoAr)
+        {
+            return nettoAr * (1 + AfaKulcs);
+        }
+
+        public RaktarElem(string cikkNev, int dbSzam)
+        {
+            CikkNev = cikkNev;
+            this.dbSzam = dbSzam;
+        }
+
+    }
+}
