e88265fdf0930d2dabcc90a8800738bc4fa03b5a
MolnarMarcell <marci20070208@gmail.com>
2026-09-14 10:25:13 +0200

    1. feladat: Webáruház Termék (Külső osztály, get, set és validáció)

---
+++ WebaruhazTermek/Program.cs
@@ -0,0 +1,6 @@
+﻿using WebaruhazTermek;
+
+Termek pozitivTermek = new Termek("Pozitív termék", 100, 10);
+Console.WriteLine($"Név: {pozitivTermek.Nev}, Egységár: {pozitivTermek.Egysegar}, Készlet: {pozitivTermek.Keszlet}");
+Termek NegativTermek = new Termek("Negatív termék", -50, -5);
+Console.WriteLine($"Név: {NegativTermek.Nev}, Egységár: {NegativTermek.Egysegar}, Készlet: {NegativTermek.Keszlet}");
---
+++ WebaruhazTermek/Termek.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebaruhazTermek
+{
+    internal class Termek
+    {
+        public string Nev { get; set; }
+
+        private int egysegar;
+        private int keszlet;
+        public int Egysegar
+        {
+            get { return egysegar; }
+            set { 
+                if (value < 1) 
+                {
+                    egysegar = 1; 
+                }
+            else
+                {
+                    egysegar = value;
+                }
+}
+        }
+
+        public int Keszlet
+        {
+            get { return keszlet; }
+            set {
+                if (value <= 0)
+                {
+                    keszlet = 0;
+                }
+                else
+                {
+                    keszlet = value;
+                }
+            }
+        }
+
+        public Termek(string nev, int egysegar, int keszlet)
+        {
+            Nev = nev;
+            Egysegar = egysegar;
+            Keszlet = keszlet;
+        }
+    }
+}
